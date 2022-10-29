04c728c7e9a9c8df05cb7fd774ff9aeb3fd23219
Alex Hortin <hortinstein@gmail.com>
2022-10-29 15:34:23 +0000

    added some scripts

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+nvm use 16
 npm install pm2 -g
 
 cd naivecoin-ui/naivecoin-explorer
@@ -6,12 +9,12 @@ pm2 --name explorer start npm -- start
 
 cd ../../naivecoin-ui/naivecoin-wallet
 npm install
-pm2 --name waller start npm -- start
+pm2 --name wallet start npm -- start
 
-cd ../../naivecoin-ui/naivecoin-wallet
+cd ../../naivecoin
 
 #start the node
-cd naivecoin/
 npm install
-npm start
+pm2 --name node start npm -- start
 
+curl -H "Content-type:application/json" --data '{"peer" : "ws://159.223.96.145:6001"}' http://localhost:3001/addPeer
